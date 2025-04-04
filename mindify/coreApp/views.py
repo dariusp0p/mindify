cc9244e551a086b15607a0ccb27c6499f103e63f
dariusp0p <dariuspop2005@gmail.com>
2025-04-05 01:54:29 +0300

    initiated-front-end-bootstrap

--- mindify/coreApp/views.py
+++ mindify/coreApp/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def core(request):
+    return render(request, '../templates/core.html')
--- mindify/mindify/settings.py
+++ mindify/mindify/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 'coreApp.apps.CoreappConfig',
+    # 'userApp.apps.UserappConfig',
+    'coreApp',
+    'userApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'mindify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mindify' / 'static',  
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
