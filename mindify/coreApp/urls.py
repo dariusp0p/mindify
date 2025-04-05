a9462ac408e9d131f5e636c66607f87f7e1fda09
dariusp0p <dariuspop2005@gmail.com>
2025-04-05 13:56:35 +0300

    some-events-update

--- mindify/coreApp/urls.py
+++ mindify/coreApp/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('events/', views.events, name='events'),
     path('premium/', views.premium, name='premium'),
+    path('events/create', views.createEvent, name='create_event'),
+    path('events/view', views.viewEvent, name='view_event'),
+    path('events/checkout', views.checkout, name='checkout'),
+    path('payment/success', views.success, name='success'),
 ]
--- mindify/coreApp/views.py
+++ mindify/coreApp/views.py
@@ -8,3 +8,15 @@ def events(request):
 
 def premium(request):
     return render(request, 'coreApp/premium.html')
+
+def createEvent(request):
+    return render(request, 'coreApp/create-event.html')
+
+def viewEvent(request):
+    return render(request, 'coreApp/view-event.html')
+
+def checkout(request):
+    return render(request, 'coreApp/checkout.html')
+
+def success(request):
+    return render(request, 'coreApp/payment-success.html')
