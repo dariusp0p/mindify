f628cc6cd84054bfbb7d49359cc0d7bea3b0a017
dariusp0p <dariuspop2005@gmail.com>
2025-10-07 11:43:44 +0300

    chore: added .env.example and cleaned settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-from decouple import config
+from decouple import Config
 
 
+config = Config('.env')
 
 OPENAI_API_KEY = config('OPENAI_API_KEY', default=None)
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -18,7 +18,9 @@ SECRET_KEY = 'django-insecure-^x1!h*zpnn)$@6#avsf-d2#%s+3zjm+aye=m&d$22$*1n$qnf5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -111,14 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-import os
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
-]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -126,8 +125,3 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
